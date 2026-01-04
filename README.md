a893ad891f4d08017d94a063b17eb95dbac2e408
seesee010 <sebastian.daubner2@gmail.com>
2026-01-04 01:41:01 +0100

    2026-1-4 - 1:40 : - first code -

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Esp32-C3-SuperMini-MQTT-BME280
 Just as the title tell
+
+ESP32-C3 with BME280 sensor sending data via MQTT to Home Assistant.
+
+## License
+This project is licensed under GPL-3.0
+
+## Dependencies
+
+This project uses the following libraries:
+
+- [BME280 by Tyler Glenn](https://github.com/finitespace/BME280) - GPL-3.0
+- [AViShaMQTT by Ajang](https://github.com/avisha95/AViShaMQTT) - GPL-3.0
+
+Each library is licensed under its own terms (GPL-3.0).
+Please refer to their respective repositories for full license text.
+
+## Hardware
+- ESP32-C3 Super Mini
+- BME280 Sensor
---
+++ src/BMEMqtt/BMEMqtt.ino
@@ -0,0 +1,116 @@
+/*
+  seesee010
+  2026-01
+
+  BME + MQTT
+
+  Details:
+  Esp32 C3 Super Mini + BME280 - mqtt -> Home Assistant
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+  Dependencies:
+  - BME280I2C.h by Tyler Glenn (GPL-3.0)
+  - AViShaMQTT.h by Ajang (GPL-3.0)
+
+==========
+
+  BME + MQTT
+
+  Details:
+  Esp32 C3 Super Mini + BME280 - mqtt -> Home Assistant
+
+  Connection:
+  Sensor -> Board
+
+  Pinout:
+  VCC -> 3.3V
+  Gnd -> Gnd
+  SCL -> 9
+  SDA -> 8
+
+==========
+
+*/
+
+#define baudrate 115200
+
+#define pin_sda 8
+#define pin_scl 9
+
+// using the BME280I2C.h by Tyler Glenn
+// gpl 3.0 license
+#include <BME280I2C.h>
+
+// AViShaMQTT.h by Ajang
+// gpl 3.0 license
+#include <AViShaMQTT.h>
+
+const char *ssid = "my_wifi";
+const char *password = "password";
+const char *mqtt_server = "broker.emqx.io";
+
+const char *topicTemp = "homeassistant/outside/0/temp";
+const char *topicPres = "homeassistant/outside/0/pres";
+const char *topicHum = "homeassistant/outside/0/hum";
+
+AViShaMQTT mqtt(ssid, password, mqtt_server);
+
+BME280I2C bme;
+
+float temp(NAN), hum(NAN), pres(NAN);
+
+void getData(Stream *data) {
+
+// from BME_280_I2C_Test
+
+   BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
+   BME280::PresUnit presUnit(BME280::PresUnit_Pa);  
+
+   bme.read(pres, temp, hum, tempUnit, presUnit);
+// ##########
+
+  delay(500);
+}
+
+void sendData(float temp, float pres, float hum) {
+
+  // send -> HA Server
+  mqtt.publish(topicTemp, String(temp));
+  mqtt.publish(topicPres, String(pres));
+  mqtt.publish(topicHum, String(hum));
+}
+
+void setup() {
+
+  // bme280
+  Wire.begin(pin_sda, pin_scl);
+  bme.begin();
+
+  // inizialise Serial Interface
+  Serial.begin(baudrate);
+
+  mqtt.begin();
+}
+
+void loop() {
+
+  mqtt.loop();
+
+  // main loop
+  getData(&Serial);
+  sendData(temp, pres, hum);
+
+  delay(3500);
+}
